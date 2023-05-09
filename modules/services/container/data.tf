29230b3eb6d2a267ab23953d75b9fa0b29d25848
Saurabh Kumar <saurabh@deepfence.io>
2023-05-09 11:41:55 +0530

    fix

--- modules/services/container/data.tf
+++ modules/services/container/data.tf
@@ -3,13 +3,3 @@
 data "google_client_config" "current" {}
 
 data "google_organization" "org" {}
-
-data "google_project" "all_projects" {
-  count = length(data.google_organization.org.projects)
-
-  project_id = data.google_organization.org.projects[count.index].project_id
-}
-
-output "projects" {
-  value = [for project in data.google_project.all_projects : project.project_id]
-}
